eb5a3e50ef701336182c90ae02bd8ba2e7b5cf13
hbrashid <hbrashid@gmail.com>
2020-05-28 01:39:23 -0500

    Working, but clunky

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+class People extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLiked: false
+    };
+  }
+
+  handleClick = () => {
+    this.setState({
+      isLiked: !this.state.isLiked
+    })
+}
+
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{margin: '0 auto 60px auto', width: '500px', border: '1px solid black', padding: '20px', borderRadius: '4px'}} >
+    <img src={this.props.people.picture.large} alt={'random people'} />
+  <button onClick={this.handleClick} >{this.state.isLiked ? <div> Hide Details: {this.props.people.location.country}, {this.props.people.email}, {this.props.people.dob.age}</div> : 'Show Details'}</button>
+  <div>{this.props.people.name.first} {this.props.people.name.last}</div>
     </div>
   );
 }
+}
+
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      people: []
+    };
+  }
+
+  componentDidMount () {
+    fetch('https://randomuser.me/api?results=25')
+    .then(json => json.json())
+    .then(data => {
+      this.setState({
+        people: [...data.results]
+      })
+      console.log(data);
+    })
+  }
+  
+
+ 
+
+render() {
+return (
+  <div className="App">
+    {this.state.people.map((peopleData, index) => (
+    <People key={index} people={peopleData} />
+    ))}
+  </div>
+  );
+  }
+  }
+
 
 export default App;
